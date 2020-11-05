9840ff2b2f049b7c86f63aed3930c25880c0790d
kldoc <fred.berbigier@gmail.com>
2020-11-05 11:21:50 -0300

    SystemUI: add Pulse and Ambient notification bars [1/2]
    [maxwen] SystemUI: support using notification color for pulse light
    
    optional use the color information of the notification
    as color for notification pulse light
    
    @brituj
    * Update color when a new notification arrives while pulsing
    * Fix row == null when on lockscreen even though the notification is clearable
    * other code improvements
    
    -------------------------------------------------------------------
    [SQUASH]
    Add Pulse and Ambient Display bars for notifications
    
    Change-Id: Ic5fdbdf05ef3f836e0c42ecef30e791a1f7eeb6e
    
    SystemUI: trigger pulse light only for notifications
    
    store the doze trigger reason in a setting to check later
    if it has been triggered for a notification or any other reason
    
    Change-Id: Iecee97b04e5e943c766fb4794d89e036ded65b3a
    
    SystemUI: Pulse light accent color option
    
    Change-Id: If402e96d7549aff7f758236e6f54a6034d08f9c2
    
    SystemUI: use ROWS_HIGH_PRIORITY for active notification pulse check
    
    only one of those could make pulse happen anyway
    
    Change-Id: I002344f321bdfed770f1099d7d33edc35671d997
    
    SystemUI: replace pulse light drawable with our own vector
    --------------------------------------------------------------------
    
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: I95756d9886b2f4120d5ece72418850b100fe7394
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4849,6 +4849,24 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_DYNAMIC = "network_traffic_dynamic";
 
         /**
+         * Whether to enable PULSE Edge lights
+         * @hide
+         */
+        public static final String AMBIENT_NOTIFICATION_LIGHT = "ambient_notification_light";
+
+        /**
+         * @hide
+         */
+        public static final String AMBIENT_NOTIFICATION_LIGHT_AUTOMATIC = "ambient_notification_light_automatic";
+
+        /**
+         * helper setting to contain the current pulse reason so we can check
+         * if it has been triggered from a notification or a gesture
+         * @hide
+         */
+        public static final String PULSE_TRIGGER_REASON = "pulse_trigger_reason";
+
+        /**
          * These are all public system settings
          *
          * @hide
@@ -4960,6 +4978,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_DYNAMIC);
+            PRIVATE_SETTINGS.add(AMBIENT_NOTIFICATION_LIGHT);
+            PRIVATE_SETTINGS.add(AMBIENT_NOTIFICATION_LIGHT_AUTOMATIC);
         }
 
         /**
