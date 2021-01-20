7c7f770c14175464091f7cb0275b116b40769ee4
Manish4586 <manish.n.manish45@gmail.com>
2021-01-20 23:12:07 +0530

    fwb: set 0.5f Transition Animation Scale
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -942,7 +942,7 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManagerInternal mPowerManagerInternal;
 
     private float mWindowAnimationScaleSetting = 1.0f;
-    private float mTransitionAnimationScaleSetting = 1.0f;
+    private float mTransitionAnimationScaleSetting = 0.5f;
     private float mAnimatorDurationScaleSetting = 1.0f;
     private boolean mAnimationsDisabled = false;
     boolean mPointerLocationEnabled = false;
